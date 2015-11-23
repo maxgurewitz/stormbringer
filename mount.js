d6fa712dadc74e8fddd51b6e853d8facc962ae8f
maxgurewitz <maxthegeek1@gmail.com>
2015-11-23 01:37:25 -0800

    jsdom mount spec

--- mount.js
+++ mount.js
@@ -2,6 +2,7 @@ var mainLoop = require('main-loop');
 var diff = require('virtual-dom/vtree/diff');
 var patch = require('virtual-dom/vdom/patch');
 var create = require('virtual-dom/vdom/create-element');
+var createModelProperty = require('./create-model-property');
 
 module.exports = function mount(args) {
   var listenTo = args.listenTo || [];
@@ -9,18 +10,13 @@ module.exports = function mount(args) {
   var loop = mainLoop(args.store, args.render, {
     diff: diff,
     patch: patch,
-    create: create
+    create: create,
+    document: args.document
   });
 
-  var illegalSetMessage = 'Cannot mutate model while in render function.';
-  var viewCompatibleStore = Object.create({
-      send: args.store.send
-    }, {
-      model: {
-        get: function() { return args.store.model; },
-        set: function() { throw new Error(illegalSetMessage); }
-      }
-    });
+  var viewCompatibleStore = createModelProperty({
+    send: args.store.send
+  }, args.store.model);
 
   args.store.onUpdate(function () {
     loop.update(viewCompatibleStore);
--- test/mount.spec.js
+++ test/mount.spec.js
@@ -1,41 +1,47 @@
-var document = require('min-doc');
-var h = require('virtual-dom');
-var event = require('synthetic-event');
+var raf = require('raf');
+var jsdom = require('jsdom').jsdom;
 var helpers = require(__BASE + '/test/helpers');
-var mount = require(__BASE + '/src/mount');
-var buildStore = require(__BASE + '/src/buildStore');
+var mount = require(__BASE + '/mount');
+var buildStore = require(__BASE + '/build-store');
+var h = require(__BASE + '/h');
+
+var document = jsdom();
+var window = document.defaultView;
+var expect = helpers.expect;
 
 describe('mount', function() {
-  function render(store) {
-    return h('div', {
-      'ev-click', function() {
-        store.send({ type: 'increment' });
-      }
-    }, store.model.count + ' click');
-  }
-
-  var store = buildStore({
-    update: function(model, action) {
-      switch (action.type) {
-        case 'increment':
-          model.count++;
-          return model;
-        default:
-          return model;
-      }
-    },
-    model: {
-      count: 0
-    }
-  });
+  var app;
 
   before(function() {
-    var div = document.createElement('div');
-    mount({store: store, el: div, render: render, listenTo: ['mouseover']});
-    div.dispatchEvent(event({ type: 'click' }));
+    var store = buildStore({
+      model: { count: 0 },
+      update: function(model, action) {
+        if (action.type === 'increment') {
+          model.count++;
+        }
+        return model;
+      }
+    });
+
+    app = mount({
+      document: document,
+      el: document.documentElement,
+      store: store,
+      render: function(store) {
+        return h('div', { id: 'app' }, 'count is : ' + store.model.count);
+      }
+    });
   });
 
-  it('renders the correct output', function() {
-    expect(div.innerHtml).to.equal(h('div', '1 click'));
+  it('renders the correct text', function(done) {
+    var renderedText = document.querySelector('#app').textContent;
+    expect(renderedText).to.contain('count is : 0');
+    app.send({ type: 'increment' });
+
+    raf(function() {
+      renderedText = document.documentElement.textContent;
+      expect(renderedText).to.contain('count is : 1');
+      done();
+    });
   });
 });
